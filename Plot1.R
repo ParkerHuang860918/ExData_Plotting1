e9e9b4e423df9f4827ae891b9fe84a62929fa17b
ParkerHuang860918 <huangfeiyu120@hotmail.com>
2017-09-02 11:41:09 -0400

    Create Plot1.R

---
+++ Plot1.R
@@ -0,0 +1,8 @@
+Plot1.R
+
+raw_data<-"./household_power_consumption.txt"
+project<-read.table(raw_data,header=TRUE, sep=";", stringsAsFactors=FALSE)
+subset<-project[project$Date %in% c("1/2/2007","2/2/2007"), ]
+png("plot1.png", width=480, height=480)
+hist(as.numeric(subset$Global_active_power), col="red", main="Global Active Power", xlab="Global Active Power (kilowatts)")
+dev.off()
